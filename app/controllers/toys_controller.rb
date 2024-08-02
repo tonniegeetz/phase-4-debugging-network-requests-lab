cb7bafcea3c1f1d42734dc40e5da143dcf410dd2
tonniegeetz <tonniegeetz@gmail.com>
2024-08-02 12:02:18 +0300

    debugging

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,13 +7,18 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    if toy
+      toy.update(toy_params)
+      render json: toy, status: :accepted
+    else
+      render json: { error: 'toy not found' }, status: :not_found
+    end
   end
 
   def destroy
@@ -23,9 +28,8 @@ class ToysController < ApplicationController
   end
 
   private
-  
+
   def toy_params
     params.permit(:name, :image, :likes)
   end
-
 end
